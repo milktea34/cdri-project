9d6f6fb340a59fc9d933ce46882024ab1494efd7
serena <serena@08liter.com>
2022-07-31 22:45:19 +0900

    네이버 인증 헤더 별도 값으로 지정

--- src/api/BookAPI.ts
+++ src/api/BookAPI.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const naverAuthHeader:any = {
+    'X-Naver-Client-Id': 'spT7e4fr9Fv0p2YvkrOX',
+    'X-Naver-Client-Secret': 'TlD7RpKb9p'
+}
+
 export async function getBookList(query: string, start: number) {
     let url = '/api/v1/search/book.json?query=' + query;
     const response = await axios.get(url, {
-        headers: {
-            'X-Naver-Client-Id': 'spT7e4fr9Fv0p2YvkrOX',
-            'X-Naver-Client-Secret': 'TlD7RpKb9p'
-        },
+        headers: naverAuthHeader,
         params: {start: (start-1) * 10 + 1}
     })
     return response.data;
@@ -15,10 +17,7 @@ export async function getBookList(query: string, start: number) {
 export async function getBookListDetail(query: string, start: number) {
     let url = '/api/v1/search/book_adv.json?' + query;
     const response = await axios.get(url, {
-        headers: {
-            'X-Naver-Client-Id': 'spT7e4fr9Fv0p2YvkrOX',
-            'X-Naver-Client-Secret': 'TlD7RpKb9p'
-        },
+        headers: naverAuthHeader,
         params: {start: (start-1) * 10 + 1}
     })
     return response.data;
